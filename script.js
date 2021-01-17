83a87854ea5bad97914def09e8baeb257a854ee0
Leah <leahtiktin@gmail.com>
2021-01-17 11:10:11 -0700

    Create initial etch and sketch projects

---
+++ script.js
@@ -0,0 +1,68 @@
+
+const para = document.querySelector('.para')
+let grid = document.querySelector('.grid');
+let squares = document.querySelectorAll('.square');
+let squaresArr = Array.from(squares)
+squares.forEach((square) => { 
+square.addEventListener('mouseenter', function blue(){
+    square.classList.add('blue');
+}
+)
+
+});
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    clearGrid();
+    newGrid();
+});
+    function clearGrid(){
+        squares.forEach((square) => {
+        square.classList.remove('blue');
+        square.classList.add('square')
+    
+    })
+       
+    }
+      
+  function newGrid(){
+    let numberSquares = Array.from(squares).length;
+      let gridPreference = prompt('Pick your grid preference:', '4');
+      let totalGrid = gridPreference * gridPreference;
+      grid.style.cssText = `grid-template-columns: repeat(${gridPreference}, 1fr);
+      grid-template-rows: repeat(${gridPreference}, 1fr);`
+      if(gridPreference>100){
+           para.textContent = 'Please enter a number less than 100';
+      }
+      else if(totalGrid>numberSquares){
+          for(let i = numberSquares; i<totalGrid; i++){
+            newSquare = document.createElement('div');
+            newSquare.classList.add('square');
+            newSquare.classList.add('newSquares');
+              grid.appendChild(newSquare);
+        }
+  }
+  else{
+      
+      for(let i = numberSquares; i>totalGrid; i--){
+          
+      removedSquare = squaresArr[squaresArr.length - 1];
+      removedSquare.classList.remove('square');
+      squaresArr.pop();
+      console.log(squares)
+      }
+      squares.forEach((square)=>{
+          
+      })
+   squaresArr = Array.from(squares);
+      
+  }
+  squares = document.querySelectorAll('.square');
+  squares.forEach((square) => { 
+   square.addEventListener('mouseenter', function blue(){
+       square.classList.add('blue');
+   }
+   )
+   
+   });
+   console.log(squares)
+  }
